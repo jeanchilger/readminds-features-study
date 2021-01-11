4a1b84d780740e152a50d2b8cbca64b5100718af
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2021-01-10 21:36:57 -0300

    docs: improves documentation

--- src/features/face/face_analyzer.h
+++ src/features/face/face_analyzer.h
@@ -1,6 +1,8 @@
 #ifndef _READMINDS_FEATURES_FACE_FACE_ANALYZER_H_
 #define _READMINDS_FEATURES_FACE_FACE_ANALYZER_H_
 
+#include <deque>
+
 #include "mediapipe/framework/formats/landmark.pb.h"
 #include "src/features/generic_analyzer.h"
 
@@ -24,19 +26,36 @@ class FaceAnalyzer : public GenericAnalyzer {
         double GetFaceMotion();
 
     protected:
-        double face_area_;
-        double face_motion_;
-        int num_frames_motion_ = 50;
+        double face_area_ = -1;
+        double face_motion_ = -1;
 
-        // Calls all other update functions.
-        void Update();
+        int num_frames_motion_ = 50;
+        std::deque<mediapipe::NormalizedLandmarkList> last_anchors_;
 
         // Calculates the area of the convex hull that closes
         // the whole face.
+        //
+        // Relative to feature F5 in Fernando's paper.
         void CalculateFaceArea();
 
+        // Calculates the Euclidean distance between the last 
+        // `num_frames_motion` anchor landmarks. The distance is computed
+        // between the first anchor list and all others.
         //
+        // Represents the amount of movement the head perfored in a short
+        // period of time.
+        //
+        // Relative to feature F6 in Fernando's paper.
         void CalculateFaceMotion();
+
+        // Calls all other update functions.
+        void Update() override;
+
+        // Updates the list containing the last `num_frames_motion_` anchor
+        // lists. If less than this number of frames have ocurred, it simply
+        // adds a new anchor list, otherwise it inserts the new and removes
+        // the oldest.
+        void UpdateLastAnchors();
 
 
 };
