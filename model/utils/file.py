734c6485ac038784091b32278457a4ffbf6e12cf
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2021-01-28 21:25:40 -0300

    feat: adds path creation tool

--- model/utils/file.py
+++ model/utils/file.py
@@ -1,4 +1,20 @@
+import os
 import csv
+
+
+def resolve_path(path):
+    """Creates folders for given path, if they don't exists.
+
+    Args:
+        path (path): Path to be created.
+    """
+
+    full_path = ""
+    for name in path.split("/"):
+        if "." not in name:
+            full_path += name + "/"
+            if not os.path.exists(full_path):
+                os.mkdir(full_path)
 
 
 def save_results_to_csv(file_path, results, headers):
@@ -10,9 +26,11 @@ def save_results_to_csv(file_path, results, headers):
         headers (list of str): Header names.
     """
 
+    resolve_path(file_path)
+
     with open(file_path, "w") as output_file:
         csv_writer = csv.writer(output_file)
         csv_writer.writerow(headers)
 
-        for row in resuts:
+        for row in results:
             csv_writer.writerow(row)
