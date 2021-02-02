1194a2bf372639bbe833739240f106fbaca5aa9b
Junior V. Ramisch <junior.ramisch@gmail.com>
2021-02-01 23:21:10 -0300

    chore: adds argparse test

--- src/features/hr/main.py
+++ src/features/hr/main.py
@@ -2,86 +2,98 @@ from video_wrapper import VideoWrapper
 from ica_estimator import ICAEstimator
 import argparse
 
-
-def test_ica_estimator():
+def create_parser():
     parser = argparse.ArgumentParser(description="Main script for ica \
-                                                  method testing. Please \
-                                                  use check --help")
+                                                  method testing.")
 
-    parser.addArgument("--video_path",
+    parser.add_argument("--video_path",
                        type=str,
+                       required=True,
                        help="Path to the video input.")
 
-    parser.addArgument("--detector",
+    parser.add_argument("--detector",
                        type=str,
+                       required=True,
                        help="Face detector method \
-                             dlib, mtcnn, mtcnn_kalmann).")
+                             (dlib, mtcnn, mtcnn_kalmann).")
 
-    parser.addArgument("--extractor",
+    parser.add_argument("--extractor",
                        type=str,
+                       required=True,
                        help="Video extractor (opencv, skvideo).")
 
-    parser.addArgument("--type_roi",
+    parser.add_argument("--type_roi",
                        type=str,
+                       required=True,
                        help="Method for ROI extraction \
                             (skin_adapt, skin_fix, rect).")
 
-    parser.addArgument("--type_roi",
-                       type=str,
-                       help="Method for ROI extraction \
-                             (skin_adapt, skin_fix, rect).")
-
-    parser.addArgument("--skin_thresh_adapt",
+    parser.add_argument("--skin_thresh_adapt",
                        type=float,
                        help="Skin threshold needed when \
                              type_roi=skin_adapt.")
 
-    parser.addArgument("--skin_thresh_fix",
+    parser.add_argument("--skin_thresh_fix",
                        type=float,
                        help="Skin threshold needed when \
                              type_roi=skin_fix.")
 
-    parser.addArgument("--rect_coords",
+    parser.add_argument("--rect_coords",
                        type=list,
                        help="ROI coordinates, needed when \
                              type_roi=skin_fix and no \
                              rect_regions is provided.")
 
-    parser.addArgument("--rect_regions",
+    parser.add_argument("--rect_regions",
                        type=list,
                        help="ROI regions, needed when type_roi=rect \
                              and no rect_coords is provided.")
 
-    parser.addArgument("--end_time",
+    parser.add_argument("--end_time",
                        type=int,
                        help="End time in seconds, \
                              needed when pyVHR is not able to compute \
-                             video duration with amount_of_frame / fps.")
+                             video duration like amount_of_frame / fps.")
 
-    args = parser.parse_args()
+    return parser
 
-    video_path = args.video_path
-    detector = args.detector
-    extractor = args.extractor
-    type_roi = args.type_roi
-    skin_thresh_adapt = args.skin_thresh_adapt
-    skin_thresh_fix = args.skin_thresh_fix
-    rect_coords = args.rect_coords
-    rect_regions = args.rect_regions
-    end_time = args.end_time
-    face_extractor = VideoWrapper(video_path, detector, extractor, type_roi,
-                                  skin_thresh_adapt=skin_thresh_adapt,
-                                  skin_thresh_fix=skin_thresh_fix,
-                                  rect_coords=rect_coords,
-                                  rect_regions=rect_regions,
-                                  end_time=59)
+
+def test_face_extractor(args):
+    face_extractor = VideoWrapper(args.video_path, args.detector, 
+                                  args.extractor, args.type_roi,
+                                  skin_thresh_adapt=args.skin_thresh_adapt,
+                                  skin_thresh_fix=args.skin_thresh_fix,
+                                  rect_coords=args.rect_coords,
+                                  rect_regions=args.rect_regions,
+                                  end_time=args.end_time)
     face_extractor.extract_faces()
     face_extractor.show_faces()
+
+def test_ica_estimator(args):
+    print(args.video_path) 
+    print(args.detector)
+    print(args.extractor)
+    print(args.type_roi)
+    print(args.skin_thresh_adapt)
+    print(args.skin_thresh_fix)
+    print(args.rect_coords)
+    print(args.rect_regions)
+    print(args.end_time)
+    exit()   
+    face_extractor = VideoWrapper(args.video_path, args.detector, 
+                                  args.extractor, args.type_roi,
+                                  skin_thresh_adapt=args.skin_thresh_adapt,
+                                  skin_thresh_fix=args.skin_thresh_fix,
+                                  rect_coords=args.rect_coords,
+                                  rect_regions=args.rect_regions,
+                                  end_time=args.end_time)
 
     ica = ICAEstimator(face_extractor)
     ica.run_offline()
     ica.save("miaau.csv")
 
-
 if __name__ == "__main__":
-    test_ica_estimator()
+    parser = create_parser()
+    args = parser.parse_args()
+    #test_face_extractor(args)
+    test_ica_estimator(args)
